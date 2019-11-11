b3a61275041022763d3e7ab45c4df6dba4dd979d
Al Schaapman <me@alschaapman.com>
2019-11-11 14:37:47 -0800

    Format console output as error

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const config = require('./config');
 
 const client = new Client(config);
 client.connect()
-  .then(() => console.log('DATABASE CONNECTION SUCCESSFUL.'))
-  .catch((err) => console.log(`DATABASE CONNECTION FAILED WITH ERR ${err}`));
+  .then(() => console.log('DATABASE CONNECTION SUCCESSFUL'))
+  .catch((err) => console.error(`DATABASE CONNECTION FAILED WITH ERR ${err}`));
 
 module.exports = client;
